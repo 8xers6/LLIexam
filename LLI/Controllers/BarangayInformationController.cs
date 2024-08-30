27bd7f8361544c225fa8c3b158df635a1f55baf7
8xers6 <joemariejacinto1930@gmail.com>
2024-08-30 23:43:57 +0800

    Update/Edit button module done!

--- LLI/Controllers/BarangayInformationController.cs
+++ LLI/Controllers/BarangayInformationController.cs
@@ -1,6 +1,7 @@
 ﻿using LLI.Data;
 using LLI.Models;
 using LLI.Models.Entities;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Net;
@@ -60,6 +61,24 @@ namespace LLI.Controllers
             return View(resident);
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Edit(BarangayInformation viewModel)
+        {
+            var resident = await dbContext.BrgyInformation.FindAsync(viewModel.Id);
+            if(resident is not null){
+
+                resident.Name = viewModel.Name;
+                resident.Birthday = viewModel.Birthday;
+                resident.Address = viewModel.Address;
+                resident.Phone = viewModel.Phone;
+                resident.EmergencyContactName = viewModel.EmergencyContactName;
+                resident.EmergencyContact = viewModel.EmergencyContact;
+                resident.Email = viewModel.Email;
+
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "BarangayInformation");
+        }
 
 
 
