11d1eec1d0a8d6ac84b806723fe8b4d244c2000c
8xers6 <joemariejacinto1930@gmail.com>
2024-08-31 01:08:41 +0800

    nothing happen

---
+++ LLI/Data/DbContext.cs
@@ -0,0 +1,31 @@
+﻿using LLI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LLI.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure the Users entity
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.UserId);
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.PasswordHash)
+                    .IsRequired();
+            });
+        }
+    }
+}
