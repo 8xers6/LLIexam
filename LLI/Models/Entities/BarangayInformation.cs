b19c96e4ea4b56a9aa529b430128cf2fb04f7b56
8xers6 <joemariejacinto1930@gmail.com>
2024-08-30 22:19:07 +0800

    setting up the database

---
+++ LLI/Models/Entities/BarangayInformation.cs
@@ -0,0 +1,18 @@
+﻿namespace LLI.Models.Entities
+{
+    public class BarangayInformation
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Birthday { get; set; }
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string EmergencyContactName { get; set; }
+        public string EmergencyContact { get; set; }
+        public string Email { get; set; }
+    }
+}
