76e1a7b886cf76ce8c7800b3e16528d56af4191b
8xers6 <joemariejacinto1930@gmail.com>
2024-08-30 23:01:47 +0800

    Success on adding Data on Database using Add Module

--- LLI/Controllers/BarangayInformationController.cs
+++ LLI/Controllers/BarangayInformationController.cs
@@ -1,18 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LLI.Data;
+using LLI.Models;
+using LLI.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Numerics;
 
 namespace LLI.Controllers
 {
     public class BarangayInformationController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public BarangayInformationController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
             return View();
         }
         [HttpPost]
-        public IActionResult Add()
+        public async Task<IActionResult> Add(AddResidentViewModel viewModel)
         {
+            var resident = new BarangayInformation
+            {
+                Name = viewModel.Name,
+                Birthday = viewModel.Birthday,
+                Address = viewModel.Address,
+                Phone = viewModel.Phone,
+                EmergencyContactName = viewModel.EmergencyContactName,
+                EmergencyContact = viewModel.EmergencyContact,
+                Email = viewModel.Email
+            };
 
+            await dbContext.BrgyInformation.AddAsync(resident);
+            await dbContext.SaveChangesAsync();
+
+
+            return View();
         }
     }
 }
--- LLI/Models/AddResidentViewModel.cs
+++ LLI/Models/AddResidentViewModel.cs
@@ -2,5 +2,18 @@
 {
     public class AddResidentViewModel
     {
+        public string Name { get; set; }
+
+        public string Birthday { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string EmergencyContactName { get; set; }
+
+        public string EmergencyContact { get; set; }
+
+        public string Email { get; set; }
     }
 }
